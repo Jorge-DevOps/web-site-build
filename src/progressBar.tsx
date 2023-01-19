062f972f2bca5dd70cc6ec80203c292992ad6342
jarangel <jarangel@indracompany.com>
2023-01-18 19:53:23 -0500

    :construction: function
    Progress bar

--- src/progressBar.tsx
+++ src/progressBar.tsx
@@ -1,9 +1,10 @@
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { useEffect, useRef, useState } from 'react';
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 10,
+    height: 15,
     borderRadius: 5,
     width: '60%',
     position: 'sticky',
@@ -14,15 +15,35 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
     },
     [`& .${linearProgressClasses.bar}`]: {
         borderRadius: 5,
-        backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+        backgroundColor: theme.palette.mode === 'light' ? '#b3642d' : '#b3642d',
     },
 }));
 
 
 export default function CustomizedProgressBars() {
+
+    const [progress, setProgress] = useState(0);
+    let timer: any = useRef();
+
+    useEffect(() => {
+        if (!timer.current) {
+            timer.current = setInterval(() => {
+                setProgress((prev) => {
+                    if (prev < 20) {
+                        return prev + 1;
+                    }
+                    if (prev === 100) {
+                        clearInterval(timer.current);
+                    }
+                    return prev;
+                });
+            }, 100);
+        }
+    }, []);
+
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <BorderLinearProgress variant="determinate" value={20} />
+            <BorderLinearProgress variant="determinate" value={progress} />
         </Box>
     );
 }
