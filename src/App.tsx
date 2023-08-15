a49478c24ff960275313bc3b43642cff45a871e8
Jorge Rangel <jarangel@indracompany.com>
2023-08-15 15:41:25 -0500

    feat(new web site)

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import CustomizedProgressBars from './progressBar'
 import site from './assets/6261569.jpg'
+import mobileSite from './assets/6261479.jpg'
 import instagram from './assets/instagram.svg'
 import youtube from './assets/youtube.svg'
 import facebook from './assets/facebook.svg'
 
 
 function App() {
-
   return (
     <>
-      <div className='container-web-site' >
+      <div className='container-web-site'>
         <div className='container-title'>
           <h1 className='title'>Sitio web en <br /> construcción</h1>
-
         </div>
-        <img src={site} style={{ filter: 'blur(8px)' }} className="webSite" alt="React logo" />
+        <img style={{ filter: 'blur(8px)' }} src={site} className="webSite" alt="Web" />
+        <img style={{ filter: 'blur(8px)' }} src={mobileSite} className="webSite-mobile" alt="Mobile Web" />
         <div className='container-social-media'>
           <a href="https://www.instagram.com/cesarramirezabogados/">
-
-            <img src={instagram} className="social-media" alt="React logo" />
+            <img src={instagram} className="social-media" alt="Instagram" />
           </a>
-          {/* <a href="https://www.youtube.com/">
-            <img src={youtube} className="social-media" alt="React logo" />
-          </a>
-          <a href="https://www.facebook.com/javieralfonsoabogados/">
-            <img src={facebook} className="social-media" alt="React logo" />
-          </a> */}
         </div>
       </div>
       <CustomizedProgressBars />
     </>
-
-  )
-}
+  );
+};
 
 export default App
